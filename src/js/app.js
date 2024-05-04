2026a6b171238c89e98b881a7144be49cefbba4b
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-05-04 15:45:04 -0300

    update modals service

--- src/js/app.js
+++ src/js/app.js
@@ -36,4 +36,38 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-
+// Função para abrir o modal com as informações
+function openModal(content) {
+    $('#modal-content').html(content);
+    $('#myModal').css('display', 'block');
+  }
+  
+  // Quando o usuário clicar fora do modal, feche-o
+  $(window).click(function(event) {
+    if (event.target == $('#myModal')[0]) {
+      $('#myModal').css('display', 'none');
+    }
+  });
+  
+  // Ao clicar em um item da lista, exiba o modal com as informações relevantes
+  $('ul li a').click(function(e) {
+    e.preventDefault(); // Previne o comportamento padrão do link
+    var service = $(this).data('service'); // Obtém o atributo data-service do link clicado
+    var content = ''; // Aqui você adicionará o conteúdo do modal com base no serviço clicado
+  
+    // Verifica qual serviço foi clicado e atribui o conteúdo apropriado
+    switch(service) {
+      case 'lp':
+        content = 'A Licença Prévia (LP) é um documento que autoriza a viabilidade ambiental de um empreendimento ou atividade, indicando sua localização e concepção.';
+        break;
+      case 'li':
+        content = 'A Licença de Instalação (LI) é um documento que autoriza o início da construção, montagem, instalação, ampliação, alteração ou reforma de um empreendimento.';
+        break;
+      // Adicione os casos para os outros serviços aqui
+      default:
+        content = 'Descrição não encontrada.';
+    }
+  
+    openModal(content); // Abre o modal com o conteúdo correspondente
+  });
+  
